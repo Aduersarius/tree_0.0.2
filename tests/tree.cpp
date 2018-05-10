230cc00475571ef98daf181c03d65a798babf5bd
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-05-10 17:42:43 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -10,7 +10,7 @@ TEST_CASE("insert,print and remove")
     tree_t<int> tree{10,4,16,2,13,18,3,17};
     tree.help_print(ostream);
     cout << "removing 10:";
-    tree.remove(10)
+    tree.remove(10);
     cout << tree.find(10)<<'\n';
     tree.help_print(ostream);
     REQUIRE(input+input1==ostream.str());
