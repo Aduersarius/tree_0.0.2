81bc606143007b2aa0b04e867f79f350d56f51dd
Aduersarius <31540232+Aduersarius@users.noreply.github.com>
2018-05-11 12:20:37 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -4,14 +4,14 @@ using namespace std;
 #include "tree.hpp"
 
 TEST_CASE("insert,print and remove")
-{   string input{"------------18\n----------------17\n--------16\n------------13\n----10\n--------4\n----------------3\n------------2\n"};
-    string input1{"------------18\n----------------17\n--------16\n----13\n--------4\n----------------3\n------------2\n"};
+{   string input{"------------15\n----------------12\n--------11\n------------8\n----7\n--------4\n----------------3\n------------2\n"};
+    string input1{"------------15\n----------------12\n--------11\n----7\n--------4\n----------------3\n------------2\n"};
     std::ostringstream ostream;
-    tree_t<int> tree{10,4,16,2,13,18,3,17};
+    tree_t<int> tree{7,4,11,2,8,15,3,12};
     tree.help_print(ostream);
-    cout << "removing 10:";
-    tree.remove(10);
-    cout << tree.find(10)<<'\n';
+    cout << "removing 8:";
+    tree.remove(8);
+    cout << tree.find(8)<<'\n';
     tree.help_print(ostream);
     REQUIRE(input+input1==ostream.str());
    
@@ -19,8 +19,8 @@ TEST_CASE("insert,print and remove")
 TEST_CASE("compare"){
      
  std::ostringstream ostream;
-    tree_t<int> tree{10,4,16,2,13,18,3,17};
-    tree_t<int> tree1{10,4,16,2,13,18,3,17};
+    tree_t<int> tree{7,4,11,2,8,15,3,12};
+    tree_t<int> tree1{7,4,11,2,8,15,3,12};
     ostream<<(tree==tree1?"equal\n":"not equal\n");
     tree1.insert(8);
     tree1.insert(9);
